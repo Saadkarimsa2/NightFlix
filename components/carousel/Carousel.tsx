6c0964730a3f25f2f866c7edb2df80b2ba23829b
Saad karim <mdsaad2018@gmail.com>
2024-08-25 21:46:25 +0530

    Update Carousel.tsx

--- components/carousel/Carousel.tsx
+++ components/carousel/Carousel.tsx
@@ -1,71 +1,65 @@
-import React, {useState} from "react";
+// Carousel.tsx
+import React, { useState } from "react";
 import { IoIosArrowBack } from 'react-icons/io';
-import CarouselItem, { CardProps } from "./CarouselItem";
+import CarouselItem from "./CarouselItem";
 import CarouselIndicator from "./CarouselIndicator";
 
-export interface CarouselProps{
+export interface CarouselProps {
     width?: number;
     height?: number;
     items: React.ReactNode[];
 }
 
-function Carousel({width, height, items} : CarouselProps) {
-
-    const[activeIndex , setActiveIndex] = useState<number>(0);
+function Carousel({ width, height, items }: CarouselProps) {
+    const [activeIndex, setActiveIndex] = useState<number>(0);
 
     function handlePrevItemBtn() {
         setActiveIndex((prev) => {
-            return prev - 1 >=0? prev-1 : prev;
-        })
+            return prev - 1 >= 0 ? prev - 1 : prev;
+        });
     }
-
-    // function to handle clicking the next button 
 
     function handleNextItemBtn() {
         setActiveIndex((prev) => {
-            return prev + 1 < items.length? prev+1 : prev;
-        })
+            return prev + 1 < items.length ? prev + 1 : prev;
+        });
     }
 
     return (
         <div className='carousel-container mt-10'>
-          {activeIndex > 0 &&(
-            <button className='carousel-btn-switch-card-left carousel-btn-switch-card'
-            onClick={handlePrevItemBtn}><IoIosArrowBack /></button>
-          )}
+            {activeIndex > 0 && (
+                <button className='carousel-btn-switch-card-left carousel-btn-switch-card'
+                    onClick={handlePrevItemBtn}>
+                    <IoIosArrowBack />
+                </button>
+            )}
 
-          {items?.map((item, index) => (
-            <CarouselItem key={ index } index={index} activeIndex={activeIndex}>
-                {item}
-            </CarouselItem>
-          ))}
+            {items?.map((item, index) => (
+                <CarouselItem key={index} index={index} activeIndex={activeIndex}>
+                    {item}
+                </CarouselItem>
+            ))}
 
-          {/* next button */}
+            {activeIndex < items.length - 1 && (
+                <button className='carousel-btn-switch-card-right carousel-btn-switch-card'
+                    onClick={handleNextItemBtn}>
+                    <IoIosArrowBack 
+                        style={{
+                            transform: 'rotate(180deg)',
+                        }}
+                    />
+                </button>
+            )}
 
-          {activeIndex < items.length - 1 && (
-            <button className='carousel-btn-switch-card-right carousel-btn-switch-card'
-            onClick={handleNextItemBtn}
-            >
-             <IoIosArrowBack 
-             style={
-                {
-                    transform: 'rotate(180deg)',
-                }
-             }/>
-            </button>
-          )}
-
-          <CarouselIndicator
-          activeIndex={activeIndex}
-          length={items.length}
-        onSetActiveIndex={(activeIndex) =>{
-            setActiveIndex(activeIndex);
-        }}
-          >
-
-          </CarouselIndicator>
+            <CarouselIndicator
+                activeIndex={activeIndex}
+                length={items.length}
+                onSetActiveIndex={(activeIndex) => {
+                    setActiveIndex(activeIndex);
+                }}
+            />
         </div>
-    )
+    );
 }
 
-export default Carousel
+export default Carousel;
